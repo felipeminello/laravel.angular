94e890c797893d94691599c027661cb804e5fc6a
Felipe Minello <felipeminello@gmail.com>
2015-12-05 04:10:19 -0200

    Fase 1 - Criando Model e Migration - Criando primeiro registro - Factory Client - ClientTableSeeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,10 +12,20 @@
 */
 
 $factory->define(CodeProject\User::class, function (Faker\Generator $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
-    ];
+	return [
+		'name' => $faker->name,
+		'email' => $faker->email,
+		'password' => bcrypt(str_random(10)),
+		'remember_token' => str_random(10),
+	];
+});
+
+$factory->define(CodeProject\Client::class, function (Faker\Generator $faker) {
+	return [
+		'responsable' => $faker->name,
+		'email' => $faker->email,
+		'phone' => $faker->phoneNumber,
+		'address' => $faker->address,
+		'obs' => $faker->sentence,
+	];
 });
