b18c1de3b3c88d3c76419b2ade1a87addf9c0bbf
Felipe Minello <felipeminello@gmail.com>
2015-12-05 14:19:52 -0200

    Fase 1 - Alteração do client migration e seed: inserindo o campo name - Criação do ClientController e CRUD - Remoção do CSRF

--- app/Client.php
+++ app/Client.php
@@ -6,12 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    protected $fillable = [
-	    'name',
-	    'responsable',
-	    'email',
-	    'phone',
-	    'address',
-	    'obs'
-    ];
+	protected $fillable = [
+		'name',
+		'responsable',
+		'email',
+		'phone',
+		'address',
+		'obs'
+	];
 }
