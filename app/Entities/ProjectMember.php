180c9ab4ce2d71397b281e14c56591e269ff0121
Felipe Minello <felipeminello@gmail.com>
2015-12-25 19:31:50 -0200

    Relacionando ProjectMembers

---
+++ app/Entities/ProjectMember.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CodeProject\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+class ProjectMember extends Model implements Transformable
+{
+    use TransformableTrait;
+
+    protected $fillable = [
+		'project_id',
+		'member_id'
+	];
+
+	public function project()
+	{
+		return $this->belongsTo(Project::class);
+	}
+
+	public function member()
+	{
+		return $this->belongsTo(User::class, 'member_id');
+	}
+
+}
