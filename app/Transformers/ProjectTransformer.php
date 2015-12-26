36d8bab6726f71310ca3791dd7ad6149486b32b9
Felipe Minello <felipeminello@gmail.com>
2015-12-26 15:14:40 -0200

    Includes

--- app/Transformers/ProjectTransformer.php
+++ app/Transformers/ProjectTransformer.php
@@ -7,15 +7,28 @@ use League\Fractal\TransformerAbstract;
 
 class ProjectTransformer extends TransformerAbstract
 {
+	protected $defaultIncludes = ['members', 'client'];
+
 	public function transform(Project $project)
 	{
 		return [
 			'project_id' => $project->id,
+			'owner_id' => $project->owner_id,
 			'project' => $project->name,
 			'description' => $project->description,
 			'progress' => $project->progress,
 			'status' => $project->status,
 			'due_date' => $project->due_date
 		];
+	}
+
+	public function includeMembers(Project $project)
+	{
+		return $this->collection($project->members, new ProjectMemberTransformer());
+	}
+
+	public function includeClient(Project $project)
+	{
+		return $this->collection($project->client()->get(), new ProjectClientTransformer());
 	}
 }
