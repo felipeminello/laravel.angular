be8f13e053102592e223c0e74d9938e210271b3e
Felipe Minello <felipeminello@gmail.com>
2015-12-26 23:15:01 -0200

    Declarando dependências no gulpfile - Parte 2

--- gulpfile.js
+++ gulpfile.js
@@ -14,13 +14,21 @@ config.build_path_js = config.build_path + '/js';
 config.build_vendor_path_js = config.build_path_js + '/vendor';
 config.vendor_path_js = [
     config.bower_path + '/jquery/dist/jquery.min.js',
-    config.bower_path + '/bootstrap/dist/js/bootstrap.min.js'
+    config.bower_path + '/bootstrap/dist/js/bootstrap.min.js',
+    config.bower_path + '/angular/angular.min.js',
+    config.bower_path + '/angular-route/angular-route.min.js',
+    config.bower_path + '/angular-resource/angular-resource.min.js',
+    config.bower_path + '/angular-animate/angular-animate.min.js',
+    config.bower_path + '/angular-messages/angular-messages.min.js',
+    config.bower_path + '/angular-bootstrap/angular-bootstrap.min.js',
+    config.bower_path + '/angular-bootstrap/ui-bootstrap.min.js',
+    config.bower_path + '/angular-strap/dist/modules/navbar.min.js'
 ];
 config.build_path_css = config.build_path + '/css';
 config.build_vendor_path_css = config.build_path_css + '/vendor';
 config.vendor_path_css = [
     config.bower_path + '/bootstrap/dist/css/bootstrap.min.css',
-    config.bower_path + '/bootstrap/dist/css/bootstrap-theme.min.css',
+    config.bower_path + '/bootstrap/dist/css/bootstrap-theme.min.css'
 ];
 
 /*
