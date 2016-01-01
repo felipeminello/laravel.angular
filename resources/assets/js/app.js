a73cb6487eafe07a9df8a7b2acced2b6e0e42133
Felipe Minello <felipeminello@gmail.com>
2016-01-01 17:34:30 -0200

    Realizando o primeiro login

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,8 +1,8 @@
-var app = angular.module('app', ['ngRoute', 'app.controllers']);
+var app = angular.module('app', ['ngRoute', 'angular-oauth2', 'app.controllers']);
 
-angular.module('app.controllers', []);
+angular.module('app.controllers', ['angular-oauth2']);
 
-app.config(function ($routeProvider) {
+app.config(['$routeProvider', 'OAuthProvider', function ($routeProvider, OAuthProvider) {
     $routeProvider
         .when('/login', {
             templateUrl: 'build/views/login.html',
@@ -12,4 +12,29 @@ app.config(function ($routeProvider) {
             templateUrl: 'build/views/home.html',
             controller: 'HomeController'
         });
-});
+
+    OAuthProvider.configure({
+        baseUrl: 'http://laravel.angular',
+        clientId: 'appid1',
+        clientSecret: 'secret',
+        grantPath: 'oauth/access_token'
+    });
+
+}]);
+
+app.run(['$rootScope', '$window', 'OAuth', function ($rootScope, $window, OAuth) {
+    $rootScope.$on('oauth:error', function (event, rejection) {
+        // Ignore `invalid_grant` error - should be catched on `LoginController`.
+        if ('invalid_grant' === rejection.data.error) {
+            return;
+        }
+
+        // Refresh token when a `invalid_token` error occurs.
+        if ('invalid_token' === rejection.data.error) {
+            return OAuth.getRefreshToken();
+        }
+
+        // Redirect to `/login` with the `error_reason`.
+        return $window.location.href = '/login?error_reason=' + rejection.data.error;
+    });
+}]);
--- resources/assets/js/controllers/login.js
+++ resources/assets/js/controllers/login.js
@@ -1,11 +1,15 @@
 angular.module('app.controllers')
-    .controller('LoginController', ['$scope', function ($scope) {
+    .controller('LoginController', ['$scope', '$location', 'OAuth', function ($scope, $location, OAuth) {
         $scope.user = {
             username: '',
             password: ''
         };
 
         $scope.login = function () {
-
+            OAuth.getAccessToken($scope.user).then(function () {
+                $location.path('home');
+            }, function() {
+                alert('login inválido');
+            });
         };
     }]);
