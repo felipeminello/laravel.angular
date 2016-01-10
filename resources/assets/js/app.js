538ac467dd6b1dc7777698ab57d30a7a7c09c612
Felipe Minello <felipeminello@gmail.com>
2016-01-10 15:58:40 -0200

    Excluindo cliente

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -37,6 +37,10 @@ app.config(['$routeProvider', 'OAuthProvider', 'OAuthTokenProvider', 'appConfigP
         .when('/clients/:id/edit', {
             templateUrl: 'build/views/client/edit.html',
             controller: 'ClientEditController'
+        })
+        .when('/clients/:id/remove', {
+            templateUrl: 'build/views/client/remove.html',
+            controller: 'ClientRemoveController'
         });
 
     OAuthProvider.configure({
