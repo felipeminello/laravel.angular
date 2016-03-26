a1983b72d58774ec6c8d49a157559f09e701c818
Felipe Minello <felipeminello@gmail.com>
2016-03-25 23:04:41 -0300

    Configurando css do layout na página de boot

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,10 @@
     <title>Laravel</title>
 
     @if(Config::get('app.debug'))
-        <link href="{{ asset('build/css/vendor/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('build/css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('build/css/components.css') }}" rel="stylesheet">
+        <link href="{{ asset('build/css/flaticon.css') }}" rel="stylesheet">
+        <link href="{{ asset('build/css/font-awesome.css') }}" rel="stylesheet">
         <link href="{{ asset('build/css/vendor/bootstrap-theme.min.css') }}" rel="stylesheet">
     @else
         <link href="{{ elixir('css/all.css') }}" rel="stylesheet">
