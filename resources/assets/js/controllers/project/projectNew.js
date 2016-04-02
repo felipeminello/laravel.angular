df06326522ef6f2b560375f32de9f9979c933187
Felipe Minello <felipeminello@gmail.com>
2016-04-02 19:53:56 -0300

    Criando lista de seleção de clients

--- resources/assets/js/controllers/project/projectNew.js
+++ resources/assets/js/controllers/project/projectNew.js
@@ -1,6 +1,11 @@
 angular.module('app.controllers')
-    .controller('ProjectNewController', ['$scope', '$location', 'Project', function ($scope, $location, Project) {
+    .controller('ProjectNewController',
+    ['$scope', '$location', 'Project', 'Client', function ($scope, $location, Project, Client) {
+
         $scope.project = new Project();
+        $scope.clients = Client.query();
+
+        
 
         $scope.save = function () {
             if ($scope.form.$valid) {
