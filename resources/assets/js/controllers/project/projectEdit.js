d2c25bc33b8adcd9097912a8959bc0aa6c0b58cc
Felipe Minello <felipeminello@gmail.com>
2016-04-09 19:45:20 -0300

    Typeahead - edição de projetos

--- resources/assets/js/controllers/project/projectEdit.js
+++ resources/assets/js/controllers/project/projectEdit.js
@@ -1,20 +1,50 @@
 angular.module('app.controllers')
     .controller('ProjectEditController',
-        ['$scope', '$location', '$cookies', '$routeParams', 'Project', 'Client', 'appConfig', function ($scope, $location, $cookies, $routeParams, Project, Client, appConfig) {
+        ['$scope', '$location', '$cookies', '$routeParams', 'Project', 'Client', 'appConfig',
+            function ($scope, $location, $cookies, $routeParams, Project, Client, appConfig) {
 
-            $scope.project = Project.get({id: $routeParams.id});
-            $scope.clients = Client.query();
-            $scope.status = appConfig.project.status;
+                Project.get({id: $routeParams.id}, function (data) {
+                    $scope.project = data;
+                    $scope.project.progress = parseInt($scope.project.progress);
 
-            $scope.project.progress = parseInt($scope.project.progress);
-
-            $scope.save = function () {
-                if ($scope.form.$valid) {
-                    $scope.project.owner_id = $cookies.getObject('user').id;
-
-                    Project.update({id: $scope.project.id}, $scope.project, function() {
-                        $location.path('/projects');
+                    Client.get({id: data.client_id}, function (data) {
+                        $scope.clientSelected = data;
                     });
-                }
+                });
+
+                $scope.status = appConfig.project.status;
+
+
+
+                $scope.save = function () {
+                    if ($scope.form.$valid) {
+                        $scope.project.owner_id = $cookies.getObject('user').id;
+
+                        Project.update({id: $scope.project.id}, $scope.project, function () {
+                            $location.path('/projects');
+                        });
+                    }
+                };
+
+                $scope.formatName = function (model) {
+                    if (model) {
+                        return model.name;
+                    }
+
+                    return '';
+                };
+
+                $scope.getClients = function (name) {
+                    return Client.query({
+                        search: name,
+                        searchFields: 'name:like'
+                    }).$promise;
+                };
+
+                $scope.selectClient = function (item) {
+                    $scope.project.client_id = item.id;
+                };
+
             }
-        }]);
+        ]
+    );
