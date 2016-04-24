515a8f7a916809a9f2a521fd00e5b26ea46b599e
Felipe Minello <felipeminello@gmail.com>
2016-04-23 21:50:52 -0300

    Implementando logica de upload no projectFileNew

--- resources/assets/js/controllers/project-file/projectFileNew.js
+++ resources/assets/js/controllers/project-file/projectFileNew.js
@@ -1,6 +1,6 @@
 angular.module('app.controllers')
     .controller('ProjectFileNewController',
-        ['$scope', '$location', '$routeParams', 'ProjectFile', 'Project', function ($scope, $location, $routeParams, ProjectFile, Project) {
+        ['$scope', '$location', '$routeParams', 'ProjectFile', 'Project', 'Upload', function ($scope, $location, $routeParams, ProjectFile, Project, Upload) {
             
         $scope.projectFile = new ProjectFile();
         $scope.projectFile.project_id = $routeParams.id;
@@ -13,9 +13,28 @@ angular.module('app.controllers')
 
         $scope.save = function () {
             if ($scope.form.$valid) {
+                Upload.upload({
+                    url: 'upload/url',
+                    data: {
+                        name: $scope.projectFile.name,
+                        description: $scope.projectFile.description,
+                        file: $scope.projectFile.file
+                    }
+
+                }).then(function (resp) {
+                    console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+                }, function (resp) {
+                    console.log('Error status: ' + resp.status);
+                }, function (evt) {
+                    var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+                    console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+                });
+
+/*
                 $scope.projectFile.$save({id: $routeParams.id}).then(function () {
                     $location.path('/project/' + $routeParams.id + '/files');
                 });
+*/
             }
         }
     }]);
